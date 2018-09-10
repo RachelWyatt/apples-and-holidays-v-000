a85f4aef3143f53c213c4a4ce759f8710f5e3e8a
Rachel Wyatt <rw126@evansville.edu>
2018-09-10 22:56:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, season_hash|
     season_hash.collect do |holiday, supplies|
       if supplies.include? "BBQ"
-        return holiday
+       holiday
       end
     end
   end
