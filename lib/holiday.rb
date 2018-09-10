b17f84543109807cdbc9832ddb530cda3dafb1db
Rachel Wyatt <rw126@evansville.edu>
2018-09-10 22:13:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,7 +37,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, season_hash|
     puts season.to_s.capitalize.insert(-1, ":")
     season_hash.each do |holiday, supplies|
-      holiday_formatted = holiday.to_s.capitalize
+      holiday_formatted = holiday.to_s.tr("_"," ").capitalize
       supplies_formatted = supplies.join(", ")
       puts "  #{holiday_formatted}: #{supplies_formatted}"
     end
